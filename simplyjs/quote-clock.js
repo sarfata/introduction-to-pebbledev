c5d4a28572e08aa0a2e3dbf83d995581536070d3
Thomas Sarlandie <thomas@sarlandie.net>
2014-03-04 17:57:38 -0800

    add the clock part in quote clock

--- simplyjs/quote-clock.js
+++ simplyjs/quote-clock.js
@@ -1,5 +1,20 @@
-// Clear the screen
-simply.text({}, true);
+function displayTime() {
+    var now = new Date();
+      simply.text({
+            title: "         " + now.getHours() + ":"
+              + now.getMinutes()
+              + ":" + now.getSeconds()
+          }, true);
+}
+
+// FullScreen mode
+simply.fullscreen(true);
+
+// Display time now
+displayTime();
+
+// And update it every second
+setInterval(displayTime, 1000);
 
 // Fetch a quote
 ajax({ 
